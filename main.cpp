5e6172da65b427e24c9bb57d3cec0e50cb7f3ad0
Dee Kay <dee@dee-Inspiron-N5110.(none)>
2013-03-20 02:31:19 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+inline float triangle_area(float base, float height)
+
+{
+
+float area;
+area = (0.5 * base * height);
+return area;
+}
+int main(void)
+{
+
+float b, h, a;
+cout<<"Enter the value of b";
+cin>>b;
+cout<<"Enter the value of h";
+cin>>h;
+a = triangle_area(b, h);
+cout<<"\nArea = "<<a<<endl;
+    return 0;
+}
